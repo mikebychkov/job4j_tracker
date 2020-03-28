73573ac8cd644e8e76b5c3d23b7acbbb323090bb
MikeBychkov <MihailBichkov@yandex.ru>
2020-03-28 18:06:20 +0500

    9. Написать тесты на StartUI.[#257066]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -48,4 +48,14 @@ public class StartUITest {
         Item replaced = tracker.findById(item.getId());
         assertNull(replaced);
     }
+
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
+    }
 }
