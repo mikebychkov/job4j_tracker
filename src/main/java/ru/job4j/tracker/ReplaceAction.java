f3666af415e2a8f1a14ebc60c6bb40e2593a632c
MikeBychkov <MihailBichkov@yandex.ru>
2020-03-30 15:36:00 +0500

    ReplaceAction #20200330-2

--- src/main/java/ru/job4j/tracker/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -9,18 +9,13 @@ public class ReplaceAction implements UserAction {
     @Override
     public boolean execute(Input input, Tracker tracker) {
         String id = input.askStr("Enter ID:");
-        Item res = tracker.findById(id);
-        if (res != null) {
-            Item newItem = new Item(
-                    input.askStr("Enter name:")
-            );
-            if (tracker.replace(id, newItem)) {
-                System.out.println("Successfully replaced!");
-            } else {
-                System.out.println("Something went wrong!");
-            }
+        Item newItem = new Item(
+                input.askStr("Enter name:")
+        );
+        if (tracker.replace(id, newItem)) {
+            System.out.println("Successfully replaced!");
         } else {
-            System.out.println("Wrong ID!");
+            System.out.println("Something went wrong!");
         }
         return true;
     }
