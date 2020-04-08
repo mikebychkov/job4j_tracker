5fee86ab882544ed810ee7b84164a4e51b7cd972
MikeBychkov <MihailBichkov@yandex.ru>
2020-04-08 12:24:59 +0500

    0. Сортировка[#257031] #2

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -36,6 +36,18 @@ public class Item implements Comparable<Item> {
         return this.name.compareTo(item.name);
     }
 
+    public static SortByNameAsc getAscComparator() {
+        return new SortByNameAsc();
+    }
+
+    private static class SortByNameAsc implements Comparator<Item> {
+
+        @Override
+        public int compare(Item o1, Item o2) {
+            return o1.name.compareTo(o2.name);
+        }
+    }
+
     public static SortByNameDesc getDescComparator() {
         return new SortByNameDesc();
     }
--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -33,6 +33,27 @@ public class ItemTest {
     }
 
     @Test
+    public void sortAsc() {
+        Item i1 = new Item("Cockroach");
+        Item i2 = new Item("Wrestle");
+        Item i3 = new Item("Anabel");
+
+        List<Item> items = new ArrayList<Item>();
+        items.add(i1);
+        items.add(i2);
+        items.add(i3);
+
+        List<Item> exp = new ArrayList<Item>();
+        exp.add(i3);
+        exp.add(i1);
+        exp.add(i2);
+
+        Collections.sort(items, Item.getAscComparator());
+
+        assertThat(items, is(exp));
+    }
+
+    @Test
     public void sortDesc() {
         Item i1 = new Item("Cockroach");
         Item i2 = new Item("Wrestle");
